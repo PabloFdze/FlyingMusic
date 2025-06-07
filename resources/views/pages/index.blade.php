d6fa7f92b51553e312be9c8ca724d7b9a0376da4
Pablo Fdez <pablofdezarj@gmail.com>
2025-06-07 03:50:49 +0200

    Una tontería

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>FlyingMusic</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
+   
     <style>
         body {
             background: linear-gradient(to bottom right, #000000, #383737);
